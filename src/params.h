f3190893899f6dc25b2f63d654356713472a3979
Ilia (Elias) Motornyi <elijah.mot@gmail.com>
2023-06-18 21:59:51 +0300

    Parameters fine-tunning

--- src/params.h
+++ src/params.h
@@ -5,12 +5,16 @@
 #ifndef RPI_PICO_SERVO_PARAMS_H
 #define RPI_PICO_SERVO_PARAMS_H
 //degrees
+//Stop moving when the axle angle is less or equal than:
 #define ANGLE_TOLERANCE  (2)
+//Start moving when the axle angle is more than:
 #define DEAD_ANGLE  (5)
-#define SLOW_ANGLE  (40)
+//Slow down the motor when actual angle is close to the target
+#define SLOW_ANGLE  (20)
 
+//Inverted values, i.e. 100 => no power, 80 => 20% power
 #define NO_PWM  (100)
 #define SLOW_PWM  (70)
-#define FAST_PWM  (20)
+#define FAST_PWM  (0)
 
 #endif //RPI_PICO_SERVO_PARAMS_H
